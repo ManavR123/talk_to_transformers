60f0f7031bc85aa9f83d306f28445de5af139851
Management <manav.rathod@berkeley.edu>
2021-05-09 17:41:58 -0700

    added option to optimize every x epochs, half precision training, parallel gpu training

--- training/train.py
+++ training/train.py
@@ -19,9 +19,11 @@ random.seed(0)
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, required=True, help="Which dataset to use.")
 parser.add_argument("--use_gpu", action="store_true", help="Use gpu if available")
+parser.add_argument("--use_half_precision", action="store_true", help="Use half precision")
 parser.add_argument("--size", type=str, default="medium", help="Size of base model: medium or large")
 parser.add_argument("--num_epochs", type=int, default=3, help="Number of epochs of training")
 parser.add_argument("--learning_rate", type=float, default=1e-5, help="Learning rate.")
+parser.add_argument("--optim_every", type=int, default=1, help="Learning rate.")
 parser.add_argument(
     "--optimizer_name", type=str, default="adam", help="Two options: `adam` and `lamb`",
 )
@@ -39,7 +41,9 @@ if args.use_gpu:
         print("GPU NOT AVAILABLE! USING CPU")
 
 tokenizer = AutoTokenizer.from_pretrained(f"microsoft/DialoGPT-{args.size}")
-model = AutoModelForCausalLM.from_pretrained(f"microsoft/DialoGPT-{args.size}").to(device)
+model = AutoModelForCausalLM.from_pretrained(f"microsoft/DialoGPT-{args.size}")
+model = model.to(device)
+model.parallelize()
 
 optimizer = utils_optim.build_optimizer(
     model, optimizer_name=args.optimizer_name, learning_rate=args.learning_rate
@@ -54,25 +58,33 @@ dataloader = DataLoader(
     dataset=dataset, batch_size=1, sampler=RandomSampler(dataset), drop_last=True,
 )
 
-model.train()
-wandb.watch(model)
-for _ in tqdm(range(args.num_epochs)):
-    for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
-        past_key_values, last_tokens = None, None
-        for j, inp in enumerate(data):
-            new_inputs = tokenizer([inp[0] + " " + tokenizer.eos_token], return_tensors="pt")["input_ids"].to(device)
-            if j == 0:
-                outputs = model(input_ids=new_inputs, use_cache=True)
-            else:
-                outputs = model(input_ids=new_inputs, past_key_values=past_key_values, labels=new_inputs, use_cache=True)
-                loss = outputs.loss
-                wandb.log({"loss": loss.item()})
+def step(model, data, optimizer, device, iter_num):
+    past_key_values, last_tokens = None, None
+    loss = torch.tensor(0).float().to(device)
+    for j, inp in enumerate(data):
+        new_inputs = tokenizer([inp[0] + " " + tokenizer.eos_token], return_tensors="pt", max_length=100, truncation=True)["input_ids"].to(device)
+        if j == 0:
+            outputs = model(input_ids=new_inputs, use_cache=True)
+        else:
+            outputs = model(input_ids=new_inputs, past_key_values=past_key_values, labels=new_inputs, use_cache=True)
+            loss += outputs.loss
 
-            past_key_values = outputs.past_key_values
-        
-        loss.backward()
+        past_key_values = outputs.past_key_values[-100:]
+    
+    wandb.log({"loss": loss.item()})
+    loss.backward()
+    if iter_num % args.optim_every == 0:
         optimizer.step()
         optimizer.zero_grad()
 
-model.save_pretrained(os.path.join("models", f"{args.dataset}-{args.size}"))
-tokenizer.save_pretrained(os.path.join("models", f"{args.dataset}-{args.size}"))
+model = model.train()
+if args.use_half_precision:
+    model = model.half()
+wandb.watch(model)
+for _ in tqdm(range(args.num_epochs)):
+    for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
+        step(model, data, optimizer, device, i)
+
+        if i % 1000 == 0:
+            model.save_pretrained(os.path.join("models", f"{args.dataset}-{args.size}"))
+            tokenizer.save_pretrained(os.path.join("models", f"{args.dataset}-{args.size}"))
